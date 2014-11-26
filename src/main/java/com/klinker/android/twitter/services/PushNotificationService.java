7ab51558447f6d5e67afc0453511eed8f259d1b0
Luke Klinker <lklinker1@gmail.com>
2014-11-26 16:08:36 -0600

    [New] Push Notifications
    Added a few services and a super simple backend to just try to connect and push a message to my device. Didn't work.
    
    In all honesty, I have no idea what I am doing with web stuff. It says it sent the message to my device from the page, buuttt, I don't even know how to test that. I do know that it sucessfully registers your android device when you start up the app, but that is about all I know.
    
    What works:
    - register the device. At least locally it gives it a valid identifier
    - broadcasts and services are set up to recieve messages
    
    What doesn't work:
    - Backend. Everything about it.
    
    If you want to test, the backend is set up at http://talon-twitter.appspot.com. It uses google's app engine template for GCM, but I must be doing something wrong.

---
+++ src/main/java/com/klinker/android/twitter/services/PushNotificationService.java
@@ -0,0 +1,52 @@
+package com.klinker.android.twitter.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.klinker.android.twitter.receivers.PushNotificationReceiver;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PushNotificationService extends IntentService {
+
+    public PushNotificationService() {
+        super("PushNotificationService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.v("GoogleCloudMessaging", "received message");
+
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        // The getMessageType() intent parameter must be the intent you received
+        // in your BroadcastReceiver.
+        String messageType = gcm.getMessageType(intent);
+
+        if (extras != null && !extras.isEmpty()) {  // has effect of unparcelling Bundle
+            // Since we're not using two way messaging, this is all we really to check for
+            if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+                Logger.getLogger("GCM_RECEIVED").log(Level.INFO, extras.toString());
+
+                showToast(extras.getString("message"));
+            }
+        }
+        PushNotificationReceiver.completeWakefulIntent(intent);
+    }
+
+    protected void showToast(final String message) {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
---
+++ src/main/java/com/klinker/android/twitter/ui/GCMRegisterActivity.java
@@ -0,0 +1,169 @@
+package com.klinker.android.twitter.ui;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.klinker.android.twitter.registration.Registration;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public abstract class GCMRegisterActivity extends Activity {
+
+    private static final String TAG = "GoogleCloudMessaging";
+
+    public static final String EXTRA_MESSAGE = "message";
+    public static final String PROPERTY_REG_ID = "registration_id";
+    private static final String PROPERTY_APP_VERSION = "appVersion";
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+    private static final String SENDER_ID = "282854021656";
+
+    private GoogleCloudMessaging gcm;
+    private String regid;
+
+    private SharedPreferences prefs;
+
+    private Context mContext;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mContext = getApplicationContext();
+
+        // Check device for Play Services APK.
+        if (checkPlayServices()) {
+            // If this check succeeds, proceed with normal processing.
+            // Otherwise, prompt user to get valid Play Services APK.
+            gcm = GoogleCloudMessaging.getInstance(this);
+            regid = getRegistrationId(mContext);
+
+            if (regid.isEmpty()) {
+                registerInBackground();
+            } else {
+                Log.v(TAG, regid);
+            }
+        }
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private String getRegistrationId(Context context) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            Log.i(TAG, "Registration not found.");
+            return "";
+        }
+        // Check if app was updated; if so, it must clear the registration ID
+        // since the existing regID is not guaranteed to work with the new
+        // app version.
+        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion) {
+            Log.i(TAG, "App version changed.");
+            return "";
+        }
+        return registrationId;
+    }
+
+    private SharedPreferences getGCMPreferences(Context context) {
+        // This sample app persists the registration ID in shared preferences, but
+        // how you store the regID in your app is up to you.
+        return getSharedPreferences(GCMRegisterActivity.class.getSimpleName(),
+                Context.MODE_PRIVATE);
+    }
+
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // should never happen
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+
+    /**
+     * Registers the application with GCM servers asynchronously.
+     * Stores the registration ID and app versionCode in the application's
+     * shared preferences.
+     */
+    private void registerInBackground() {
+        new AsyncTask() {
+
+
+            @Override
+            protected void onPostExecute(Object msg) {
+                Toast.makeText(mContext, (String) msg, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            protected Object doInBackground(Object[] params) {
+                String msg = "";
+
+                Registration.Builder builder = new Registration.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null);
+
+                Registration regService = builder.build();
+
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(mContext);
+                    }
+
+                    regid = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID=" + regid;
+
+                    regService.register(regid);
+
+                    // Persist the regID - no need to register again.
+                    storeRegistrationId(mContext, regid);
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                }
+                return msg;
+            }
+        }.execute(null, null, null);
+    }
+
+    private void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        int appVersion = getAppVersion(context);
+        Log.i(TAG, "Saving regId on app version " + appVersion);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
+    }
+}
